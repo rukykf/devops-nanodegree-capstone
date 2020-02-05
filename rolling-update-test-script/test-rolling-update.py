d090305353973eb9e2772c4a2d16e7ae22cd52c4
Oghenerukevwe Kofi <rukykf@gmail.com>
2020-02-05 03:53:49 +0100

    chore: preparing screenshot-rolling-update

--- rolling-update-test-script/test-rolling-update.py
+++ rolling-update-test-script/test-rolling-update.py
@@ -5,7 +5,7 @@ output = open("output_response_after_rolling_update.txt", "a+")
 old_code_response = "Hello World. <br/>This is Kofi's Udacity Devops-nanodegree capstone project"
 new_code_response = "This is Kofi's Udacity Devops-nanodegree capstone project"
 response_texts = []
-for i in range(1000):
+for i in range(600):
     response = requests.get("http://a36edb92e478d11eab27f065643d7386-1589614578.eu-west-2.elb.amazonaws.com")
     if response.text == old_code_response:
         response_texts.append("OUTDATED pod response: " + response.text)
